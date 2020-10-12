b93c23e3b2e793e24d231dd6ca58a9c17f41d029
Swapnil_Scage <swapniltrivedi1456@gmail.com>
2020-10-12 10:37:12 +0530

    final commit

---
+++ DRUMKIT/drum.js
@@ -0,0 +1,19 @@
+
+function playsound(e)
+{
+        const audio=document.querySelector(`audio[data-key="${e.keyCode}"]`);
+        const key=document.querySelector(`.key[data-key="${e.keyCode}"]`);
+        if(!audio) return;
+        audio.currentTime=0;
+        audio.play();
+        console.log(key);
+        key.classList.add('playing');
+}
+function removetransition(e)
+{
+    console.log(e.propertyName);
+    this.classList.remove('playing');
+}
+const keys=document.querySelectorAll('.key');
+keys.forEach(key=>key.addEventListener('transitionend',removetransition));
+window.addEventListener('keydown',playsound);
